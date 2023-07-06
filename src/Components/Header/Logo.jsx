a84b33593b1f1fe4abe03525b62d466d1e38eb06
Cesar <cesarramez8@gmail.com>
2023-07-05 23:04:52 -0600

    Add Logo component

---
+++ src/Components/Header/Logo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'wouter'
+
+function Logo () {
+  return (
+    <section className='header-logo cursor-pointer'>
+      <Link href='/'>
+        <figure>
+          <img
+            className='w-[134px]'
+            src='https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.22.13/mercadolibre/logo__large_plus.png'
+            alt='Logo'
+          />
+        </figure>
+      </Link>
+    </section>
+  )
+}
+
+export default Logo
