51facae412b88fb2ac4e97c85f2691bf9addea8d
Cesar <cesarramez8@gmail.com>
2023-07-05 23:05:36 -0600

    Add Suscribe component

---
+++ src/Components/Header/Suscribe.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Suscribe () {
+  return (
+    <section className='header-suscribe flex justify-end cursor-pointer'>
+      <figure>
+        <img
+          className='w-[340px]'
+          src='https://http2.mlstatic.com/D_NQ_833975-MLA52670295393_112022-OO.webp'
+          alt='Suscribe'
+        />
+      </figure>
+    </section>
+  )
+}
+
+export default Suscribe
