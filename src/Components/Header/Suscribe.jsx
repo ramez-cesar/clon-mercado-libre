630e419b6f5310bb05e2e9f0741f24c713415791
Cesar <cesarramez8@gmail.com>
2023-07-18 23:36:20 -0600

    Add Link to the Suscribe component

--- src/Components/Header/Suscribe.jsx
+++ src/Components/Header/Suscribe.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Link } from 'wouter'
 
 function Suscribe () {
   return (
     <section className='header-suscribe flex justify-end cursor-pointer'>
-      <figure>
-        <img
-          className='w-[340px]'
-          src='https://http2.mlstatic.com/D_NQ_833975-MLA52670295393_112022-OO.webp'
-          alt='Suscribe'
-        />
-      </figure>
+      <Link href='/suscripciones'>
+        <a>
+          <figure>
+            <img
+              className='w-[340px]'
+              src='https://http2.mlstatic.com/D_NQ_833975-MLA52670295393_112022-OO.webp'
+              alt='Suscribe'
+            />
+          </figure>
+        </a>
+      </Link>
     </section>
   )
 }
