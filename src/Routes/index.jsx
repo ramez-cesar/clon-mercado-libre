6c645f8af639f64aed0d8babcd25a791633088ba
Cesar <cesarramez8@gmail.com>
2023-07-18 23:34:11 -0600

    Add subscription route

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'wouter'
 import Home from '../Pages/Home'
+import Subscriptions from '../Pages/Subscriptions'
 import NotFound from '../Components/NotFound'
 
 function Routes () {
   return (
     <Switch>
       <Route path='/' component={Home} />
+      <Route path='/suscripciones' component={Subscriptions} />
       <Route path='/:rest*' component={NotFound} />
     </Switch>
   )
