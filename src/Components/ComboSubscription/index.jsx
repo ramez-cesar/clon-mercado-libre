fb1e504682864c5880b7c4f1f4e02fddb7fd4c72
Cesar <cesarramez8@gmail.com>
2023-07-19 23:28:16 -0600

    Add ComboSubscription component

---
+++ src/Components/ComboSubscription/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import GeneralButton from '../GeneralButton'
+
+function ComboSubscription ({ children }) {
+  return (
+    <section className='combo-sub w-full h-full mb-6 py-12 flex justify-center'>
+      <div className='w-[460px] mr-[52px]'>
+        <article className='h-full grid content-between'>
+          <div>
+            <h1 className='text-3xl font-semibold text-white'>Sube directo al nivel 6</h1>
+            <p className='max-w-sm mt-2 text-lg leading-[22px] text-white'>Suscríbete y disfruta los mejores beneficios a un precio increíble.</p>
+            <h3 className='mt-4 text-[32px] font-semibold text-white'>$ 99/mes</h3>
+          </div>
+
+          <div className='mt-10'>
+            <GeneralButton buttonTitle='Quiero susbscribirme' />
+            <p className='mt-5 text-[11px] leading-4 text-white'>Al suscribirte, aceptas los Términos y condiciones. <br /> de Mercado Puntos. Puedes cancelar cuando quieras.</p>
+          </div>
+        </article>
+      </div>
+
+      <div className='w-[428px]'>
+        <article className='flex justify-between gap-x-4'>
+          {
+            children
+          }
+        </article>
+      </div>
+    </section>
+  )
+}
+
+export default ComboSubscription
