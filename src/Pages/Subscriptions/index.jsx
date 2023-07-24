19b53f45773c431a99f6f1bada4204a1cbde5f1b
Cesar <cesarramez8@gmail.com>
2023-07-24 12:09:11 -0600

    Add import images and components for the Banner

--- src/Pages/Subscriptions/index.jsx
+++ src/Pages/Subscriptions/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ComboSubscription from '../../Components/ComboSubscription'
+import Banner from '../../Components/Banner'
 import BenefitCard from '../../Components/BenefitCard'
+import BenefitsPoster from '../../Components/BenefitsPoster'
 import shippingBenefit from '../../assets/images/shippingBenefit.jpg'
 import quantumania from '../../assets/images/quantumania.jpg'
 import espnNBA from '../../assets/images/espnNBA.jpg'
 import shippingBenefitLogo from '../../assets/images/shippingBenefitLogo.png'
 import disneyPlusLogo from '../../assets/images/disneyPlusLogo.png'
 import starPlusLogo from '../../assets/images/starPlusLogo.png'
+import freeShipping from '../../assets/images/freeShipping.jpg'
+import sameDayShipping from '../../assets/images/sameDayShipping.jpg'
 
 function Subscriptions () {
   return (
@@ -39,6 +43,22 @@ function Subscriptions () {
           benefits='SIN CARGO'
         />
       </ComboSubscription>
+
+      <Banner bannerTitle='Beneficios en envíos'>
+        <BenefitsPoster
+          posterTitle='Envíos gratis'
+          posterDescription='En miles de productos desde $ 299'
+          imagePath={freeShipping}
+          altImagePath='Envíos gratis'
+        />
+
+        <BenefitsPoster
+          posterTitle='Envíos rápidos en el día'
+          posterDescription='Envíos en el mismo día sin costo adicional'
+          imagePath={sameDayShipping}
+          altImagePath='Envíos rápidos en el día'
+        />
+      </Banner>
     </main>
   )
 }
