4b09ae5fa843add11dfb155ddba1643818c842cf
Cesar <cesarramez8@gmail.com>
2023-07-24 12:07:11 -0600

    Add animation to the GeneralButton component

--- src/Components/GeneralButton/index.jsx
+++ src/Components/GeneralButton/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'wouter'
 function GeneralButton ({ route, buttonTitle }) {
   return (
     <Link href={route}>
-      <a className='subscribe w-fit px-7 py-4 text-sm text-white font-semibold tracking-wide cursor-pointer rounded-lg bg-[#3483fa] hover:bg-[#2968c8]'>
+      <a className='subscribe w-fit px-7 py-4 text-sm text-white font-semibold tracking-wide cursor-pointer rounded-lg duration-[.2s] ease-in bg-[#3483fa] hover:bg-[#2968c8]'>
         {buttonTitle}
       </a>
     </Link>
