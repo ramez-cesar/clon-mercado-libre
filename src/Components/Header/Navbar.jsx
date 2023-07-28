ca14e18c111e00518f866df71603866b5aaec8ca
Cesar <cesarramez8@gmail.com>
2023-07-28 13:55:49 -0600

    Add NavbarItem in Navbar component

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react'
-import { Link } from 'wouter'
 import { ProductContext } from '../../Context'
 import { NavbarItem, NavbarItemMenu } from './NavbarItem'
 import { IconMapPin, IconShoppingCart, IconChevronDown } from '@tabler/icons-react'
 import './style.css'
+
+const textColorNavLeft = '#33333399'
+const textColorNavRight = '#333'
 
 function Navbar () {
   const {
@@ -47,71 +49,62 @@ function Navbar () {
               <IconChevronDown className='w-[20px] h-[20px] stroke-1' />
             </p>
 
-            <div className='category'>
-              <NavbarItemMenu link='/electronics' itemName='Electronics' />
-              <NavbarItemMenu link='/jewelery' itemName='Jewelery' />
-              <NavbarItemMenu link='/mens-clothing' itemName='Mens clothing' />
-              <NavbarItemMenu link='/wonens-clothing' itemName='Womens clothing' />
+            <div className='menu'>
+              <NavbarItemMenu
+                link='/electronics'
+                itemName='Electronics'
+              />
+              <NavbarItemMenu
+                link='/jewelery'
+                itemName='Jewelery'
+              />
+              <NavbarItemMenu
+                link='/mens-clothing'
+                itemName='Mens clothig'
+              />
+              <NavbarItemMenu
+                link='/wonens-clothing'
+                itemName='Womens clothig'
+              />
             </div>
           </div>
 
-          <li className='h-8 flex items-end'>
-            <Link
-              href='/vender'
-              className='text-[13px] tracking-[0.5px] text-[#33333399]'
-            >
-              Vender
-            </Link>
-          </li>
-
-          <li className='h-8 flex items-end'>
-            <Link
-              href='/ayuda'
-              className='text-[13px] tracking-[0.5px] text-[#33333399]'
-            >
-              Ayuda
-            </Link>
-          </li>
+          <NavbarItem
+            link='/vender'
+            textColor={textColorNavLeft}
+            itemName='Vender'
+          />
+          <NavbarItem
+            link='/ayuda'
+            textColor={textColorNavLeft}
+            itemName='Ayuda'
+          />
         </ul>
       </section>
 
       <section className='navbar-register flex justify-end'>
         <ul className='flex gap-6'>
-          <li>
-            <Link
-              href='/registration'
-              className='text-[13px] tracking-[0.5px] text-[#333]'
-            >
-              Crear cuenta
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              href='/login'
-              className='text-[13px] tracking-[0.5px] text-[#333]'
-            >
-              Ingresa
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              href='/mi-compras'
-              className='text-[13px] tracking-[0.5px] text-[#333]'
-            >
-              Mis compras
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              href='/carrito'
-              className='text-[13px] tracking-[0.5px] text-[#333]'
-            >
+          <NavbarItem
+            link='/registration'
+            textColor={textColorNavRight}
+            itemName='Crear cuenta'
+          />
+          <NavbarItem
+            link='/login'
+            textColor={textColorNavRight}
+            itemName='Ingresa'
+          />
+          <NavbarItem
+            link='/mis-compras'
+            textColor={textColorNavRight}
+            itemName='Mis compras'
+          />
+          <NavbarItem
+            link='/carrito'
+            itemName={
               <IconShoppingCart className='text-[#333] stroke-1 cursor-pointer' />
-            </Link>
-          </li>
+              }
+          />
         </ul>
       </section>
     </>
