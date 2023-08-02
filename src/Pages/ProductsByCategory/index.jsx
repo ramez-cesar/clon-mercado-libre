5d61c58cc487744a200cf23ccb3036cb815ad641
Cesar <cesarramez8@gmail.com>
2023-08-01 23:03:03 -0600

    Add Layout to the ProductByCategory component

--- src/Pages/ProductsByCategory/index.jsx
+++ src/Pages/ProductsByCategory/index.jsx
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react'
+import { Header } from '../../Components/Header'
+import Layout from '../../Components/Layout'
+import Card from '../../Components/Card'
 import { useLocation } from 'wouter'
 import { ProductContext } from '../../Context'
-import Card from '../../Components/Card'
+import Modal from '../../Components/Modal'
 
 let products
 
@@ -21,24 +24,28 @@ function ProductsByCategory () {
   }
 
   return (
-    <div className='w-full max-w-[1200px] m-auto mt-10 relative'>
-      <div className='ml-2 flex items-baseline gap-4'>
-        <h1 className='text-2xl'>{categoryName}</h1>
-      </div>
-      <section className='cards-container w-full h-auto py-6 px-2 flex justify-between gap-3'>
-        {
-          products?.map(({ image, title, price, rating, description }) => (
-            <Card
-              key={title}
-              image={image}
-              price={price}
-              rating={rating.rate}
-              description={description}
-            />
-          ))
-        }
-      </section>
-    </div>
+    <>
+      <Header />
+      <Layout>
+        <div className='ml-2 flex items-baseline gap-4'>
+          <h1 className='text-2xl'>{categoryName}</h1>
+        </div>
+        <section className='cards-container w-full h-auto py-6 px-2 flex justify-between gap-3'>
+          {
+            products?.map(({ image, title, price, rating, description }) => (
+              <Card
+                key={title}
+                image={image}
+                price={price}
+                rating={rating.rate}
+                description={description}
+              />
+            ))
+          }
+        </section>
+        <Modal />
+      </Layout>
+    </>
   )
 }
 
