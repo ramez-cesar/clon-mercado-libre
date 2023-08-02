51b2750011e7a9d78795b86ff3814961c7e3b191
Cesar <cesarramez8@gmail.com>
2023-08-01 23:15:50 -0600

    Remove Header from the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Routes from './Routes'
-import Header from './Components/Header'
 import { ProductProvider } from './Context'
 import './App.css'
 
 function App () {
   return (
     <ProductProvider>
-      <Header />
       <Routes />
     </ProductProvider>
   )
