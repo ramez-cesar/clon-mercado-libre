2f54c7de1bc92838ad35d456b2e2302fa325e32e
Cesar <cesarramez8@gmail.com>
2023-08-01 23:02:10 -0600

    Add Layout to the Home component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
 import { Link } from 'wouter'
+import { Header } from '../../Components/Header'
+import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 import { RightButton, LeftButton } from '../../Components/ScrollButtons'
 import { ProductContext } from '../../Context'
@@ -9,36 +11,38 @@ function Home () {
   const { products } = useContext(ProductContext)
 
   return (
-    <div className='w-full max-w-[1200px] m-auto mt-10 relative'>
-      <LeftButton />
+    <>
+      <Header />
+      <Layout>
+        <LeftButton />
+        <div className='ml-2 flex items-baseline gap-4'>
+          <h1 className='text-2xl'>Ofertas</h1>
+          <Link
+            href='/all-products'
+            className='text-sm text-[#3483fa] hover:text-[#1259c3]'
+          >
+            Ver todas
+          </Link>
+        </div>
 
-      <div className='ml-2 flex items-baseline gap-4'>
-        <h1 className='text-2xl'>Ofertas</h1>
-        <Link
-          href='/all-products'
-          className='text-sm text-[#3483fa] hover:text-[#1259c3]'
-        >
-          Ver todas
-        </Link>
-      </div>
+        <section className='cards-container w-full h-auto py-6 px-2 flex justify-between gap-3'>
+          {
+            products?.map(({ image, title, price, rating, description }) => (
+              <Card
+                key={title}
+                image={image}
+                price={price}
+                rating={rating.rate}
+                description={description}
+              />
+            ))
+          }
+        </section>
 
-      <section className='cards-container w-full h-auto py-6 px-2 flex justify-between gap-3'>
-        {
-          products?.map(({ image, title, price, rating, description }) => (
-            <Card
-              key={title}
-              image={image}
-              price={price}
-              rating={rating.rate}
-              description={description}
-            />
-          ))
-        }
-      </section>
-
-      <RightButton />
-      <Modal />
-    </div>
+        <RightButton />
+        <Modal />
+      </Layout>
+    </>
   )
 }
 
