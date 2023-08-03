486ecbd0ed0fb1177fad7cecfac370622655b73d
Cesar <cesarramez8@gmail.com>
2023-08-02 23:40:23 -0600

    Add route for login

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'wouter'
 import Home from '../Pages/Home'
 import Subscriptions from '../Pages/Subscriptions'
 import ProductsByCategory from '../Pages/ProductsByCategory'
+import Login from '../Pages/Login'
 import NotFound from '../Components/NotFound'
 
 function Routes () {
@@ -11,6 +12,7 @@ function Routes () {
       <Route path='/' component={Home} />
       <Route path='/suscripciones' component={Subscriptions} />
       <Route path='/category/:name' component={ProductsByCategory} />
+      <Route path='/login' component={Login} />
       <Route path='/:rest*' component={NotFound} />
     </Switch>
   )
