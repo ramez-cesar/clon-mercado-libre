3dc85155eaa730e1b03155126a8f005f6c6b6783
Cesar <cesarramez8@gmail.com>
2023-08-04 15:32:42 -0600

    Add setting for new font in FormModal component

--- src/Components/FormModal/index.jsx
+++ src/Components/FormModal/index.jsx
@@ -15,13 +15,13 @@ function FormModal () {
     <section className='w-[670px] h-[260px] p-9 m-auto rounded-md bg-white'>
       <div>
         <div className='flex justify-between'>
-          <h1 className='text-lg text-[#000000a4] font-semibold tracking-wider'>Elige dónde recibirás tus compras</h1>
+          <h1 className='text-lg text-[rgba(0,0,0,.9)] font-semibold tracking-wider'>Elige dónde recibirás tus compras</h1>
           <IconX
             onClick={closeModal}
             className='text-[#3483FA] cursor-pointer'
           />
         </div>
-        <p className='mt-1 mb-12 text-sm text-[#0000006b]'>Podrás ver costos y tiempos de entrega precisos en todo lo que busques.</p>
+        <p className='mt-1 mb-12 text-xs text-[rgba(0,0,0,.55)]'>Podrás ver costos y tiempos de entrega precisos en todo lo que busques.</p>
       </div>
       <Form />
     </section>
