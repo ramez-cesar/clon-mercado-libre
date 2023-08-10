6410c54f2c9e6b00fbcefebf342f35f1959fc6b2
Cesar <cesarramez8@gmail.com>
2023-08-09 22:41:04 -0600

    Add Link to Card component

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,27 +1,39 @@
 import React from 'react'
+import { Link } from 'wouter'
 import './style.css'
 
-function Card ({ image, title, price, rating, description }) {
+function Card ({
+  id,
+  image,
+  title,
+  price,
+  rating,
+  description
+}) {
   return (
-    <article className='pb-4 bg-white rounded-md shadow snap-center'>
-      <div>
-        <figure className='p-[15px] flex items-center justify-center'>
-          <img
-            className='w-[150px] h-[150px] object-contain'
-            src={image}
-            alt={title}
-          />
-        </figure>
-      </div>
+    <Link href={`/product/${id}/${title}`}>
+      <a>
+        <article className='pb-4 bg-white rounded-md shadow snap-center'>
+          <div className='w-[228px]'>
+            <figure className='p-[15px] flex items-center justify-center'>
+              <img
+                className='w-[150px] h-[150px] object-contain'
+                src={image}
+                alt={title}
+              />
+            </figure>
+          </div>
 
-      <div className='p-[16px] pb-[0]'>
-        <h1 className='text-2xl text-[rgba(0,0,0,.8)]'>$ {price}</h1>
-        <p className='text-sm text-[#00a650] tracking-[0] font-semibold'>Envio gratis</p>
-        <p className='mb-1 text-sm text-[#00a650] tracking-[0]'>Rating: ★ {rating}
-        </p>
-        <p className='description text-sm text-[rgba(32,32,32,0.8)] tracking-[0]'>{description}</p>
-      </div>
-    </article>
+          <div className='p-[16px] pb-[0]'>
+            <h1 className='text-2xl text-[rgba(0,0,0,.8)]'>$ {price}</h1>
+            <p className='text-sm text-[#00a650] tracking-[0] font-semibold'>Envio gratis</p>
+            <p className='mb-1 text-sm text-[#00a650] tracking-[0]'>Rating: ★ {rating}
+            </p>
+            <p className='description text-sm text-[rgba(32,32,32,0.8)] tracking-[0]'>{description}</p>
+          </div>
+        </article>
+      </a>
+    </Link>
   )
 }
 
