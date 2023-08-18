89fe3569441079321517a994e799e81e721301cb
Cesar <cesarramez8@gmail.com>
2023-08-17 22:59:21 -0600

    Change measurements in the Right and Left Scroll Button Component

--- src/Components/ScrollButtons/index.jsx
+++ src/Components/ScrollButtons/index.jsx
@@ -7,10 +7,10 @@ function RightButton () {
 
   return (
     <button
-      // 1140 es el total de pixeles que se desplaza por cada clic
-      // 2 es valor inicial del scroll y 3602 el final
-      onClick={() => rightScrollButton(1140, 2, 3602)}
-      className={`rightButton ${addClassRightBtn} w-[60px] h-[60px] items-center justify-center rounded-full bg-white shadow absolute right-[-20px] bottom-[40%] hover:shadow-lg`}
+      // 1194 es el total de pixeles que se desplaza por cada clic
+      // 0 es valor inicial del scroll y 3588 el final
+      onClick={() => rightScrollButton(1194, 0, 3588)}
+      className={`rightButton ${addClassRightBtn} w-[60px] h-[60px] items-center justify-center rounded-full bg-white shadow absolute right-[-20px] bottom-[35%] hover:shadow-lg`}
     >
       <IconChevronRight className='w-[30px] h-[30px] text-[#3483fa] stroke-1' />
     </button>
@@ -22,8 +22,8 @@ function LeftButton () {
 
   return (
     <button
-      onClick={() => leftScrollButton(1140, 2, 3602)}
-      className={`leftButton ${addClassLeftBtn} w-[60px] h-[60px] items-center justify-center rounded-full bg-white shadow absolute left-[-30px] bottom-[40%] hover:shadow-lg`}
+      onClick={() => leftScrollButton(1140, 0, 3588)}
+      className={`leftButton ${addClassLeftBtn} w-[60px] h-[60px] items-center justify-center rounded-full bg-white shadow absolute left-[-30px] bottom-[35%] hover:shadow-lg`}
     >
       <IconChevronLeft className='w-[30px] h-[30px] text-[#3483fa] stroke-1' />
     </button>
