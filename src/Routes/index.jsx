dc9a6659bb9ef5d2764690c99eaf2f3a04f359cd
Cesar <cesarramez8@gmail.com>
2023-08-23 14:02:04 -0600

    Add route for Product details

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -3,7 +3,7 @@ import { Switch, Route } from 'wouter'
 import Home from '../Pages/Home'
 import Subscriptions from '../Pages/Subscriptions'
 import ProductsByCategory from '../Pages/ProductsByCategory'
-import Details from '../Pages/Details'
+import ProductDetails from '../Pages/ProductDetails'
 import Login from '../Pages/Login'
 import NotFound from '../Components/NotFound'
 
@@ -13,7 +13,7 @@ function Routes () {
       <Route path='/' component={Home} />
       <Route path='/suscripciones' component={Subscriptions} />
       <Route path='/category/:name' component={ProductsByCategory} />
-      <Route path='/product/:id/:name' component={Details} />
+      <Route path='/product/:id/:name' component={ProductDetails} />
       <Route path='/login' component={Login} />
       <Route path='/:rest*' component={NotFound} />
     </Switch>
