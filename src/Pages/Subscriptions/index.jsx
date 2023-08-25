c8db884e0150da37afc853a3604e18dbd66d3401
Cesar <cesarramez8@gmail.com>
2023-08-25 12:37:30 -0600

    Add Subscriptions component

--- src/Pages/Subscriptions/index.jsx
+++ src/Pages/Subscriptions/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Header } from '../../Components/Header'
-import ComboSubscription from '../../Components/ComboSubscription'
-import Banner from '../../Components/Banner'
-import BenefitCard from '../../Components/BenefitCard'
-import BenefitsPoster from '../../Components/BenefitsPoster'
+import ComboSubscription from './Components/ComboSubscription'
+import Banner from './Components/Banner'
+import BenefitCard from './Components/BenefitCard'
+import BenefitsPoster from './Components/BenefitsPoster'
 import shippingBenefit from '../../assets/images/shippingBenefit.jpg'
 import quantumania from '../../assets/images/quantumania.jpg'
 import espnNBA from '../../assets/images/espnNBA.jpg'
@@ -18,50 +18,56 @@ function Subscriptions () {
   return (
     <>
       <Header />
-      <ComboSubscription>
-        <BenefitCard
-          addClassName='free-shipping'
-          routeImagePoster={shippingBenefit}
-          altImagePoster='Envíos gratis'
-          routeImageBenefits={shippingBenefitLogo}
-          altImageBenefits='Envíos gratis'
-          benefits='ENVIOS GRATIS Y EN EL DÍA'
-        />
+      <main>
+        <section className='w-full h-full mb-6 py-12 flex justify-center bg-gradient-to-r from-indigo-950 via-purple-900 to-fuchsia-800'>
+          <ComboSubscription>
+            <BenefitCard
+              addClassName='free-shipping'
+              routeImagePoster={shippingBenefit}
+              altImagePoster='Envíos gratis'
+              routeImageBenefits={shippingBenefitLogo}
+              altImageBenefits='Envíos gratis'
+              benefits='ENVIOS GRATIS Y EN EL DÍA'
+            />
 
-        <BenefitCard
-          addClassName='disney'
-          routeImagePoster={quantumania}
-          altImagePoster='Quantumania'
-          routeImageBenefits={disneyPlusLogo}
-          altImageBenefits='Disney+'
-          benefits='SIN CARGO'
-        />
+            <BenefitCard
+              addClassName='disney'
+              routeImagePoster={quantumania}
+              altImagePoster='Quantumania'
+              routeImageBenefits={disneyPlusLogo}
+              altImageBenefits='Disney+'
+              benefits='SIN CARGO'
+            />
 
-        <BenefitCard
-          addClassName='star'
-          routeImagePoster={espnNBA}
-          altImagePoster='NBA'
-          routeImageBenefits={starPlusLogo}
-          altImageBenefits='Star+'
-          benefits='SIN CARGO'
-        />
-      </ComboSubscription>
+            <BenefitCard
+              addClassName='star'
+              routeImagePoster={espnNBA}
+              altImagePoster='NBA'
+              routeImageBenefits={starPlusLogo}
+              altImageBenefits='Star+'
+              benefits='SIN CARGO'
+            />
+          </ComboSubscription>
+        </section>
 
-      <Banner bannerTitle='Beneficios en envíos'>
-        <BenefitsPoster
-          posterTitle='Envíos gratis'
-          posterDescription='En miles de productos desde $ 299'
-          imagePath={freeShipping}
-          altImagePath='Envíos gratis'
-        />
+        <section className='max-w-[800px] mx-auto my-6 p-4'>
+          <Banner bannerTitle='Beneficios en envíos'>
+            <BenefitsPoster
+              posterTitle='Envíos gratis'
+              posterDescription='En miles de productos desde $ 299'
+              imagePath={freeShipping}
+              altImagePath='Envíos gratis'
+            />
 
-        <BenefitsPoster
-          posterTitle='Envíos rápidos en el día'
-          posterDescription='Envíos en el mismo día sin costo adicional'
-          imagePath={sameDayShipping}
-          altImagePath='Envíos rápidos en el día'
-        />
-      </Banner>
+            <BenefitsPoster
+              posterTitle='Envíos rápidos en el día'
+              posterDescription='Envíos en el mismo día sin costo adicional'
+              imagePath={sameDayShipping}
+              altImagePath='Envíos rápidos en el día'
+            />
+          </Banner>
+        </section>
+      </main>
       <Modal />
     </>
   )
