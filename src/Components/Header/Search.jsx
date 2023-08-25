801a76f15498ea401aca42b7d8deee93c49466ed
Cesar <cesarramez8@gmail.com>
2023-08-25 13:49:58 -0600

    Add changes for temporality search functionality

--- src/Components/Header/Search.jsx
+++ src/Components/Header/Search.jsx
@@ -6,7 +6,13 @@ import { IconSearch } from '@tabler/icons-react'
 let addClass
 
 function Search () {
-  const { search, setSearch, listWantedProducts, setListWantedProducts, productList } = useContext(ProductContext)
+  const {
+    search,
+    setSearch,
+    listWantedProducts,
+    setListWantedProducts,
+    productList
+  } = useContext(ProductContext)
 
   function productFinder (e) {
     const inputValue = e.target.value
@@ -14,14 +20,18 @@ function Search () {
     setSearch(inputValue)
   }
 
-  function verifyFocus (e) {
-    // onfocus es llamado cuando el elemento tiene el foco.
-    e.target.onfocus = function () {
+  addClass = (search !== '') ? 'show-item' : ''
+
+  // Si el cursor está dentro de la sección del buscador, muestra la lista de opciones de búsqueda
+  const showSearchList = (e) => {
+    if (e.type === 'mouseenter') {
       setListWantedProducts('')
     }
+  }
 
-    // onblur es llamado cuando el elemento pierde el foco.
-    e.target.onblur = function () {
+  // Si el cursor está fuera del la sección del buscador, oculta la lista de opciones de búsqueda
+  const hideSearchList = (e) => {
+    if (e.type === 'mouseleave') {
       setListWantedProducts('hide-list')
     }
   }
@@ -29,26 +39,33 @@ function Search () {
   // Se limita el resultado de la búsqueda a 6 productos (para no mostrar una lista muy larga)
   const searchResults = productList.slice(0, 6)
 
-  addClass = (search !== '') ? 'show-item' : ''
-
   return (
-    <section className='header-search'>
+    <section
+      onMouseEnter={showSearchList}
+      onMouseLeave={hideSearchList}
+      className='header-search'
+    >
       <form className='relative'>
         <input
           value={search}
           onChange={productFinder}
-          onClick={verifyFocus}
-          className='input w-[580px] text-sm text-[rgba(32,32,32,0.8)] py-[9px] px-[15px] shadow outline-none placeholder:text-[#dbdada] autofocus focus:border focus:border-[#3483fa]'
+          className='w-[580px] text-sm text-[rgba(32,32,32,0.8)] py-[9px] px-[15px] shadow outline-none placeholder:text-[#dbdada] autofocus focus:border focus:border-[#3483fa]'
           type='text'
           placeholder='Buscar productos, marcas y mas...'
         />
       </form>
 
-      <div className={`wanted-products ${addClass} ${listWantedProducts} w-[580px] bg-white absolute z-10 border border-[#d7d7d7] shadow-md`}>
+      <div
+        className={`wanted-products ${addClass} ${listWantedProducts} w-[580px] bg-white absolute z-10 border border-[#d7d7d7]`}
+      >
         <ul className='my-2'>
           {
-            searchResults.map(({ title }) => (
-              <ProductFound key={title} result={title} />
+            searchResults.map(({ id, title }) => (
+              <ProductFound
+                key={id}
+                id={id}
+                titleProduct={title}
+              />
             ))
           }
         </ul>
@@ -57,17 +74,28 @@ function Search () {
   )
 }
 
-function ProductFound ({ result }) {
+function ProductFound ({ id, titleProduct }) {
+  const {
+    setListWantedProducts
+  } = useContext(ProductContext)
+
+  const hideSearchList = () => {
+    setListWantedProducts('hide-list')
+  }
+
   return (
-    <li className='text-[#5c5c5c] relative hover:bg-[#3483fa] hover:text-white'>
+    <li
+      onClick={hideSearchList}
+      className='text-[#5c5c5c] relative hover:bg-[#3483fa] hover:text-white'
+    >
       <span className='pl-3 absolute top-2'>
         <IconSearch className='w-5 h-5 text-[#d7d7d7] stroke-1' />
       </span>
       <Link
-        href={result}
+        href={`/product/${id}/${titleProduct}`}
         className='py-2.5 pl-11 text-[13px] block'
       >
-        {result}
+        {titleProduct}
       </Link>
     </li>
   )
